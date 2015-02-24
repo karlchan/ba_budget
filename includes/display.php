c54621dede5654e8c710b2558e208a2f4b3ecf0a
Ravi Gaddipati <rgaddipa@actionet.com>
2015-02-24 11:26:37 -0500

    New tree map library and tree map image export

--- includes/display.php
+++ includes/display.php
@@ -3,8 +3,12 @@ $(document).ready(function () {
 	function sel_chart_OnChange() {
 		if ($("#sel_chart" ).val() == 5)  {
 			$("#sel_programs" ).val('0');
-			$("#sel_programType").val('0');
+			$("#sel_programType").val('0');			
+			$("#divExport").show();
+		} else {
+			$("#divExport").hide();
 		}
+		
 		if ($("#sel_chart" ).val() == 5 || $("#sel_chart" ).val() == 1) {
 			$("#sel_years").removeAttr('multiple');
 			$("#sel_years").attr('size', '1');
